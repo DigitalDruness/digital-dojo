a4fd7f513433a2de8a36bc7072dc299293a57a74
DigitalDruness <156852355+DigitalDruness@users.noreply.github.com>
2025-08-16 22:48:29 -0500

    Create firebase-config.js

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDEhTzJiImbtTNJdScLz6DxNWRAg7Rkm1A",
+  authDomain: "digital-dojo-482eb.firebaseapp.com",
+  projectId: "digital-dojo-482eb",
+  storageBucket: "digital-dojo-482eb.firebasestorage.app",
+  messagingSenderId: "112511358197",
+  appId: "1:112511358197:web:cd1df35fa9543cafbe61bc"
+};
