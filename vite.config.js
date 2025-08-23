05d76efa89beb0bc560c142b6a152c2afdb52d13
DigitalDruness <156852355+DigitalDruness@users.noreply.github.com>
2025-08-22 19:12:35 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// This is the standard configuration for a Vite + React project.
+// It tells Vite to use the official React plugin to understand
+// and compile JSX syntax and other React-specific features.
+
 export default defineConfig({
   plugins: [react()],
-})
+});
